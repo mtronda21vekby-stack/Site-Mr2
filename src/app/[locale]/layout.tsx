a8f7bb681746133b4ee621a13f0e01adad2d2af5
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-04-19 23:20:10 -0400

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,12 @@
+import MobileStickyCall from '@/components/layout/MobileStickyCall'
+import { getGlobalSettings, type Locale } from '@/lib/content'
+
+const allowedLocales: Locale[] = ['en', 'es', 'ru']
+
+function normalizeLocale(value: string): Locale {
+  return allowedLocales.includes(value as Locale) ? (value as Locale) : 'en'
+}
+
 export default async function LocaleLayout({
   children,
   params,
@@ -5,6 +14,18 @@ export default async function LocaleLayout({
   children: React.ReactNode
   params: Promise<{ locale: string }>
 }) {
-  await params
-  return <>{children}</>
+  const { locale } = await params
+  const safeLocale = normalizeLocale(locale)
+  const global = getGlobalSettings()
+
+  return (
+    <>
+      {children}
+      <MobileStickyCall
+        locale={safeLocale}
+        phoneDisplay={global.phoneDisplay}
+        phonePrimary={global.phonePrimary}
+      />
+    </>
+  )
 }
