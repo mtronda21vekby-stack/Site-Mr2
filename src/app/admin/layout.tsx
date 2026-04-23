f2b4e029eeb139084181558b4c7ab45ea45841de
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-04-23 19:03:14 -0400

    Wrap children in AdminShell component

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,10 @@
 import type { ReactNode } from 'react'
+import AdminShell from '@/components/admin/AdminShell'
 
 export default function AdminLayout({
   children,
 }: {
   children: ReactNode
 }) {
-  return <>{children}</>
+  return <AdminShell>{children}</AdminShell>
 }
