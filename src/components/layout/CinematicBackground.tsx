07031eb43723399d6ff01a0e6ec7f36d5c16014c
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-04-26 16:22:14 -0400

    Fix Next.js SSR error by moving dynamic import to client component

--- src/components/layout/CinematicBackground.tsx
+++ src/components/layout/CinematicBackground.tsx
@@ -1,13 +1,9 @@
-import dynamic from 'next/dynamic'
-
-const Background3DEngine = dynamic(() => import('@/components/background/Background3DEngine'), {
-  ssr: false,
-})
+import Background3DClient from '@/components/background/Background3DClient'
 
 export default function CinematicBackground() {
   return (
     <div className="fixed inset-0 -z-10">
-      <Background3DEngine />
+      <Background3DClient />
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_50%_40%,transparent_0%,rgba(0,0,0,0.6)_70%)]" />
     </div>
   )
